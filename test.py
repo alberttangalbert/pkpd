7c1c4e9a920dde3fd674266967ecf0a6d2f76b09
alberttangalbert <66285052+alberttangalbert@users.noreply.github.com>
2022-05-31 20:06:20 -0400

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-# =============================================================================
-# numpy_observed_data = observed_data.numpy()
-# numpy_observed_mask = observed_mask.numpy()
-# =============================================================================
-numpy_observed_tp = observed_tp.numpy()
